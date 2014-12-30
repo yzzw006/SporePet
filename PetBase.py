14a619e845b9f223629b33956418d519c678e9ef
Yang Jiagui <yzzw006@163.com>
2014-12-30 17:57:25 +0800

    Update PetBase.py

--- PetBase.py
+++ PetBase.py
@@ -1,10 +1,42 @@
+class Pet:
+    icon = "temp.png"
+    skill = [0,0,0,-1,-1,-1]
+    #private
+    _type = 0
+    _health = 10
+    _power = 10
+    _speed = 10
+    _quantity = 1.5
+    _level = 25
 
-class PetBase:
-    __life = 150
-    __power = 10
-    __speed = 10
+    #TODO: experience
+    #_exp...
     
-    #type
-    __color = 0
-    __quantity = 0
-    __skill = [0,1,2,-1,-1,-1]
+    def __init__(self):
+        self._refreshProperty()
+    
+    def setProperty(self, health=10, power=10, speed=10, quantity=1.5, level=25):
+        self._health = health
+        self._power = power
+        self._speed = speed
+        self._level = level
+        self._quantity = quantity
+        self._refreshProperty()
+    
+    def _refreshProperty(self):
+        self.health = 100 + self._health * self._level * self._quantity * 5
+        self.power = self._power * self._level * self._quantity
+        self.speed = self._speed * self._level * self._quantity
+        self.currentHealth = self.health
+    
+class PetInBattle:
+    def __init__(self, pet):
+#         if(type(pet) != Pet):
+#             return
+        self.health = pet.health
+        self.currentHealth = pet.currentHealth
+        self.power = pet.power
+        self.speed = pet.speed
+        self.skill = pet.skill[:3]
+        
+        
